90c8b7b8093621945c6c1773b88ebaf2131d48cf
Kaya Cetinsu <kayacetinsu@gmail.com>
2023-03-09 23:14:02 -0500

    update

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -15,7 +15,7 @@
       let thisForm = this;
  
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      // let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
        
       if( ! action ) {
         displayError(thisForm, 'The form action property is not set!')
@@ -29,12 +29,14 @@
 
       if(grecaptcha.getResponse().length == 0)
       {
-          displayError(thisForm, 'Please, complete the reCaptcha');
+          displayError(thisForm, 'Please, complete the reCaptcha before submitting.');
           return false;
       }
 
-      php_email_form_submit(thisForm, action, formData);
+      formData.delete("g-recaptcha-response");
       
+      php_email_form_submit(thisForm, action, formData);
+
       // if ( recaptcha ) {
       //   if(typeof grecaptcha !== "undefined" ) {
       //     grecaptcha.ready(function() {
@@ -75,6 +77,7 @@
       if (data.trim() != '') {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
+        grecaptcha.reset();
       } else {
         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
