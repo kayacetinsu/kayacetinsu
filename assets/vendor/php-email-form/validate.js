f50d19da2714ec97c4a777fe312c353815ff20e9
Kaya Cetinsu <kayacetinsu@gmail.com>
2023-03-09 23:04:38 -0500

    Transfer

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -13,9 +13,9 @@
       event.preventDefault();
 
       let thisForm = this;
-
+ 
       let action = thisForm.getAttribute('action');
-      let recaptcha = "6LcsdeYkAAAAAEKi64hyx8XWUydPAomFSd4iLuy7";
+      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
        
       if( ! action ) {
         displayError(thisForm, 'The form action property is not set!')
@@ -27,25 +27,33 @@
 
       let formData = new FormData( thisForm );
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error)
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
+      if(grecaptcha.getResponse().length == 0)
+      {
+          displayError(thisForm, 'Please, complete the reCaptcha');
+          return false;
       }
+
+      php_email_form_submit(thisForm, action, formData);
+      
+      // if ( recaptcha ) {
+      //   if(typeof grecaptcha !== "undefined" ) {
+      //     grecaptcha.ready(function() {
+      //       try {
+      //         grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+      //         .then(token => {
+      //           formData.set('recaptcha-response', token);
+      //           php_email_form_submit(thisForm, action, formData);
+      //         })
+      //       } catch(error) {
+      //         displayError(thisForm, error)
+      //       }
+      //     });
+      //   } else {
+      //     displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+      //   }
+      // } else {
+      //   php_email_form_submit(thisForm, action, formData);
+      // }
     });
   });
 
@@ -67,7 +75,6 @@
       if (data.trim() != '') {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
-        //grecaptcha.reset();
       } else {
         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
